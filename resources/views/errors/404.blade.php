a58a23324a4a63cdf65f617fe517f2114eef1e45
Arnold <arnoldtagne@gmail.com>
2017-02-05 09:40:09 +0530

    404 added

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,58 @@
+<!--
+/**
+ * Created by PhpStorm.
+ * User: Arnold
+ * Date: 9/19/2016
+ * Time: 4:45 AM
+ */ -->
+<html>
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+		<style>
+body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    color: #B0BEC5;
+    display: table;
+    font-weight: 100;
+				font-family: 'Lato';
+			}
+
+			.container {
+    text-align: center;
+				display: table-cell;
+				vertical-align: middle;
+			}
+
+			.content {
+    text-align: center;
+				display: inline-block;
+			}
+
+			.title {
+    font-size: 72px;
+				margin-bottom: 40px;
+			}
+            .notfound{
+    font-size: 300px;
+                color: #085394;
+            }
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="content">
+				<div class="title">Page not found.</div>
+                <div class="row">
+                    <span class="notfound">4</span>
+                    <span class="notfound">0</span>
+                    <span class="notfound">4</span>
+                    <span class="notfound">!</span>
+                </div>
+			</div>
+		</div>
+	</body>
+</html>
