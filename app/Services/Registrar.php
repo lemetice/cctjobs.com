3ac27911f86c90934d8f66c35903301e511f2ed7
Arnold <arnoldtagne@gmail.com>
2017-03-11 13:26:08 +0530

    Added mail service

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -5,6 +5,7 @@ use App\User;
 use App\Models\AbonnerInfo;
 use Validator;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Mail;
 use Session;
 use Illuminate\Contracts\Auth\Registrar as RegistrarContract;
 
